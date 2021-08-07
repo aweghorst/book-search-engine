d15590bead77d25ba19106102cb6ce576047b302
aw2389 <alex2@weghorst.com>
2021-08-07 18:51:53 -0500

    added server connection

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,1 +1,6 @@
 //TODO export your typeDefs and resolvers
+
+const typeDefs = require("./typeDefs");
+const resolvers = requre("./resolvers");
+
+module.exports = { typeDefs, resolvers };
--- server/server.js
+++ server/server.js
@@ -1,23 +1,29 @@
-//TODO implement the apollo server and apply it to the express server as middleware
+const express = require("express");
+//import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
 
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+//import our typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+//create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-app.use(express.urlencoded({ extended: true }));
+//integrate our Apollo server with the express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    //log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
