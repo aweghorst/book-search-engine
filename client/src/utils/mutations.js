8bb72988317ea146aae08a1ed50f7c7d3d39e5cb
aw2389 <alex2@weghorst.com>
2021-08-07 18:53:06 -0500

    added server code

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,4 +2,4 @@
 /*LOGIN_USER will execture the loginUser mutation
 ADD_USER will execute the addUser mutation
 SAVE_BOOK will execute the saveBook mutaiton
-REMOVE_BOOK will execute the removeBook mutation  
+REMOVE_BOOK will execute the removeBook mutation  */
